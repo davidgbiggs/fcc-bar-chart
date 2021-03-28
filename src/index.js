063997f241d8cd942db4f6bb26cf9f381a026ea2
David Biggs <biggs.dg@gmail.com>
2021-03-28 01:00:32 -0500

    Fixed issues with unused variables.

--- src/index.js
+++ src/index.js
@@ -70,7 +70,7 @@ fetch(
         //    .style("top", (d3.event.pageY - 15) + "px");
         tooltip.transition().duration(50).style("opacity", 1);
       })
-      .on("mouseout", function (d, i) {
+      .on("mouseout", function () {
         d3.select(this).transition().duration("50").attr("opacity", "1");
         //Makes the new div disappear:
         tooltip.transition().duration("50").style("opacity", 0);
