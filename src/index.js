f16636786b1a010bea6f2915b8efccdf713d2b3f
David Biggs <biggs.dg@gmail.com>
2021-04-02 16:34:34 -0500

    Refactoring for best practices.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import "./styles.scss";
 import * as d3 from "d3";
 
-const w = 1000;
-const h = 500;
+const width = 950;
+const height = 550;
 const padding = 60;
 
 function generateTooltipHtml(date, gdp) {
   const dateMonth = Math.ceil((new Date(date).getMonth() + 1) / 3);
   const dateYear = new Date(date).getFullYear();
-  return `<div>Q${dateMonth} ${dateYear}</div><div>${gdp} Billion</div>`;
+  return `<div>Q${dateMonth} ${dateYear}</div><div>$${gdp} Billion</div>`;
 }
 
 fetch(
@@ -17,29 +17,52 @@ fetch(
   .then((res) => res.json())
   .then((response) => {
     const dataSet = response.data;
-    console.log(Date.parse(dataSet[0][0]));
 
+    const svg = d3
+      .select("#svg-container")
+      .append("svg")
+      .attr("width", width)
+      .attr("height", height);
+
+    // attach x axis
     const xScale = d3
       .scaleTime()
       .domain([
         d3.min(dataSet, (d) => d3.timeDay(Date.parse(d[0]))),
         d3.max(dataSet, (d) => d3.timeDay(Date.parse(d[0]))),
       ])
-      .range([padding, w - padding]);
+      .range([padding, width - padding]);
 
+    const xAxis = d3.axisBottom(xScale);
+
+    svg
+      .append("g")
+      .attr("transform", `translate(0, ${height - padding})`)
+      .attr("id", "x-axis")
+      .call(xAxis);
+
+    // attach y axis
     const yScale = d3
       .scaleLinear()
       .domain([0, d3.max(dataSet, (d) => d[1])])
-      .range([h - padding, padding]);
+      .range([height - padding, padding]);
 
     const yAxis = d3.axisLeft(yScale);
-    const xAxis = d3.axisBottom(xScale);
 
-    const svg = d3
-      .select("#svg-container")
-      .append("svg")
-      .attr("width", w)
-      .attr("height", h);
+    svg
+      .append("g")
+      .attr("transform", `translate(${padding}, 0)`)
+      .attr("id", "y-axis")
+      .call(yAxis);
+
+    // y axis label
+    svg
+      .append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", padding + 20)
+      .attr("x", -height / 2)
+      .style("text-anchor", "middle")
+      .text("Gross Domestic Product");
 
     const tooltip = d3
       .select("body")
@@ -52,41 +75,26 @@ fetch(
       .data(dataSet)
       .enter()
       .append("rect")
-      .attr("width", w / dataSet.length)
-      .attr("height", (d) => h - yScale(d[1]) - padding)
-      .attr("y", (d) => h - (h - yScale(d[1])))
+      .attr("width", width / dataSet.length)
+      .attr("height", (d) => height - yScale(d[1]) - padding)
+      .attr("y", (d) => height - (height - yScale(d[1])))
       .attr("x", (d) => xScale(Date.parse(d[0])))
       .attr("class", "bar")
+      .attr("stroke", "white")
+      .attr("stroke-width", 0.2)
       .attr("data-date", (d) => d[0])
       .attr("data-gdp", (d) => d[1])
-      .on("mouseover", (e, d) => {
-        d3.select(this).transition().duration("50").attr("opacity", ".85");
+      .on("mousemove", (e, d) => {
         tooltip
           .html(generateTooltipHtml(d[0], d[1]))
           .style("left", `${e.screenX}px`)
           .style("top", `${e.screenY - 100}px`)
           .attr("data-date", d[0]);
-        //    .style("left", (d3.event.pageX + 10) + "px")
-        //    .style("top", (d3.event.pageY - 15) + "px");
-        tooltip.transition().duration(50).style("opacity", 1);
+        tooltip.transition().duration("50").style("opacity", 1);
       })
       .on("mouseout", function () {
         d3.select(this).transition().duration("50").attr("opacity", "1");
-        //Makes the new div disappear:
+        // Makes the tooltip disappear:
         tooltip.transition().duration("50").style("opacity", 0);
       });
-
-    console.log(xScale(Date.parse(dataSet[0][0])));
-
-    svg
-      .append("g")
-      .attr("transform", `translate(${padding}, 0)`)
-      .attr("id", "y-axis")
-      .call(yAxis);
-
-    svg
-      .append("g")
-      .attr("transform", `translate(0, ${h - padding})`)
-      .attr("id", "x-axis")
-      .call(xAxis);
   });
